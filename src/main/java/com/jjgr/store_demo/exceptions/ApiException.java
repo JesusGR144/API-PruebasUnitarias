af31dce55e4b251736d2e8912c0cddc9430c20e6
Jesús Gaytán Ramírez <jesusgaytanramirez144@gmail.com>
2024-06-25 00:45:46 -0600

    Implementación de excepciones

---
+++ src/main/java/com/jjgr/store_demo/exceptions/ApiException.java
@@ -0,0 +1,17 @@
+package com.jjgr.store_demo.exceptions;
+
+import java.time.ZonedDateTime;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiException {
+    private final String message;
+    private final Throwable throwable;
+    private final HttpStatus errorCode;
+    private final ZonedDateTime timestamp;
+}
